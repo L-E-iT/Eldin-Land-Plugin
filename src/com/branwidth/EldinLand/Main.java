f0825a238a07f2105989980fb5f46b0c7a992554
Brian Elliott <ellio2bj@dukes.jmu.edu>
2017-06-29 17:06:48 -0400

    removed unnecessary files
    removed plugin.yml and creation code

--- src/com/branwidth/EldinLand/Main.java
+++ src/com/branwidth/EldinLand/Main.java
@@ -9,9 +9,6 @@ import java.io.IOException;
 
 public class Main extends JavaPlugin {
 
-    private FileConfiguration plot;
-    private File plotf;
-
     @Override
     public void onEnable() {
         // Some text here for starting the plugin
@@ -26,40 +23,13 @@ public class Main extends JavaPlugin {
     }
 
 
-    FileConfiguration getPlotConfig() {
-        return this.plot;
-    }
-
-    void savePlotData() {
-        try {
-            getPlotConfig().save(plotf);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
     private void createFiles() {
 
         File configf = new File(getDataFolder(), "config.yml");
-        plotf = new File(getDataFolder(), "plot.yml");
 
         if (!configf.exists()) {
             configf.getParentFile().mkdirs();
             saveResource("config.yml", false);
-        }
-        if (!plotf.exists()) {
-            plotf.getParentFile().mkdirs();
-            saveResource("plot.yml", false);
-        }
-
-        FileConfiguration config = new YamlConfiguration();
-        plot = new YamlConfiguration();
-        try {
-            config.load(configf);
-            plot.load(plotf);
-        } catch (IOException | InvalidConfigurationException e) {
-            e.printStackTrace();
         }
     }
 
